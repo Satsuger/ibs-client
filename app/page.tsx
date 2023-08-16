c593e36e4fc8ef1988b175aaff45f9a08c9380d8
TimaVol <tima.voloshuk@gmail.com>
2023-08-16 16:26:02 +0300

    added husky and shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,30 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 export default function Home() {
   return (
-    <main>Hello</main>
+    <main>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">Open</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </main>
   )
 }
